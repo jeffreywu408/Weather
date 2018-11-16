9f617afe9bce7ae00026852f6c90abe430515ada
Jeffrey <jeffreywu408@gmail.com>
2018-11-15 19:38:43 -0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import requests
+import re
 
 def getKey():
     apiKey = ""
@@ -17,24 +18,76 @@ def forecast(cityName):
     if (apiKey == ""):
         return
     
-    baseURL = "https://api.openweathermap.org/data/2.5/weather?"
+    #regex for ZIP Code
+    pattern = "^\d{5}(?:[-\s]\d{4})?$"
+    zip = re.match(pattern, cityName)
     
-    completeURL = baseURL + "APPID=" + apiKey + "&units=imperial" + "&q=" + cityName
+    if zip is None:
+        currentURL = "https://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&units=imperial" + "&q=" + cityName
+        w = requests.get(currentURL).json()
     
-    response = requests.get(completeURL)
-    w = response.json()
+        forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey + "&units=imperial" + "&q=" + cityName
+        f = requests.get(forecastURL).json()
+    else:
+        currentURL = "https://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&units=imperial" + "&zip=" + cityName
+        w = requests.get(currentURL).json()
+    
+        forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey + "&units=imperial" + "&zip=" + cityName
+        f = requests.get(forecastURL).json()
     
     if w["cod"] >= 200 and w["cod"] < 300: 
-        print("Current Weather Conditions")
-        print("Temperature = " + str(w["main"]["temp"]) + "F" +
-              "\nHumidity = " + str(w["main"]["humidity"]) + "%" +
-              "\nDescription = " + str(w["weather"][0]["description"]))
+        city = str(w['name'])
+        country = str(w['sys']['country'])
+        temp = str(w["main"]["temp"])
+        humidity = str(w["main"]["humidity"])
+        description = str(w["weather"][0]["description"])
+                       
+        
+        print("Current Weather Conditions in " + city + ", " + country)
+        print("Temperature = " + temp + "F" +
+              "\nHumidity = " + humidity + "%" +
+              "\nDescription = " + description + "\n")
     else:
         print(w["message"])
+        return
+    
+    currentDate = ""
+    for i in f["list"]:
+        #dt_txt is in format [YYYY-MM-DD HH:MM:SS]
+        time = i["dt_txt"]
+        
+        #nextDate = YYYY-MM-DD, hour = HH:MM:SS
+        nextDate, hour = time.split(" ")
+
+        #Only print each date once
+        if currentDate != nextDate:
+            currentDate = nextDate
+            year, month, day = currentDate.split('-')
+            date = {'y': year, 'm': month, 'd': day}
+            print('{m}/{d}/{y}'.format(**date))
+        
+        #Convert hour from 24 hour clock to 12 hour clock
+        hour = int(hour[:2])
+        if hour < 12:
+            if hour == 0:
+                hour = 12
+            print('%i:00 am' % hour)
+        else:
+            if hour > 12:
+                hour -= 12
+            print('%i:00 pm' % hour)
+        
+        temp = str(i['main']['temp'])
+        humidity = str(i["main"]["humidity"])
+        description = str(i['weather'][0]['description'])
+
+        print("Temperature = " + temp + "F" +
+              "\nHumidity = " + humidity + "%" +
+              "\nDescription = " + description + "\n")
 
 
 def main():
-    cityName = input("Enter city name: ")
+    cityName = input("Enter City Name or ZIP Code: ")
     forecast(cityName)
 
 if __name__ == '__main__':
